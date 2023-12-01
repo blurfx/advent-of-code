f5758234d8731053fd144421b39169ba5a4a5bf1
blurfx <iam@xo.dev>
2023-12-01 22:50:24 +0900

    2023 day 1, part 2

--- 2023/day1.go
+++ 2023/day1.go
@@ -30,7 +30,63 @@ func solvePart1(lines []string) int {
 	return answer
 }
 
+func solvePart2(lines []string) int {
+	answer := 0
+	regex, _ := regexp.Compile(`(\d|one|two|three|four|five|six|seven|eight|nine)`)
+
+	wordToNumber := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
+	parseNumber := func(value string) int {
+		if number, ok := wordToNumber[value]; ok {
+			return number
+		} else {
+			number, _ := strconv.Atoi(value)
+			return number
+		}
+	}
+
+	for _, line := range lines {
+		idx := 0
+		first, last := -1, -1
+		for {
+			loc := regex.FindStringIndex(line[idx:])
+			if loc == nil {
+				break
+			}
+			if first == -1 {
+				first = parseNumber(line[idx+loc[0] : idx+loc[1]])
+				last = first
+			} else {
+				last = parseNumber(line[idx+loc[0] : idx+loc[1]])
+			}
+			idx += loc[1]
+			if loc[1]-loc[0] > 1 {
+				idx -= 1
+			}
+		}
+
+		if first == -1 {
+			continue
+		}
+
+		answer += first*10 + last
+	}
+
+	return answer
+}
+
 func main() {
 	lines := readInput()
 	fmt.Println(solvePart1(lines))
+	fmt.Println(solvePart2(lines))
 }
